6635544f70ec890bf025d66ad499403e01eef073
Igor Yatsenko <igor.yatsenko09@gmail.com>
2023-05-13 18:53:41 +0300

    Update 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,9 @@
 import Notiflix from 'notiflix';
 
-// Функція createPromise з поверненням промісу
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
@@ -14,47 +13,27 @@ function createPromise(position, delay) {
   });
 }
 
-// Відправка форми
-document.getElementById("promise-form").addEventListener("submit", (event) => {
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-
   const delay = Number(event.target.elements.delay.value);
-  if (Number.isNaN(delay)) {
-    notiflix.Notify.failure('Invalid delay value!');
-    return;
-  }
-
   const step = Number(event.target.elements.step.value);
-  if (Number.isNaN(step)) {
-    notiflix.Notify.failure('Invalid step value!');
-    return;
-  }
-
   const amount = Number(event.target.elements.amount.value);
-  if (Number.isNaN(amount)) {
-    notiflix.Notify.failure('Invalid amount value!');
+
+  if (delay < 0 || step < 0 || amount < 0) {
+    Notiflix.Notify.failure('Input values cannot be negative');
     return;
   }
 
-  // Створення масиву промісів
-  const promises = [];
   for (let i = 1; i <= amount; i++) {
     const position = i;
     const promiseDelay = delay + (i - 1) * step;
-    const promise = createPromise(position, promiseDelay);
-
-    promises.push(promise);
-  }
-
-  // Відображення повідомлень користувачеві
-  Promise.allSettled(promises).then((results) => {
-    results.forEach((result) => {
-      const { position, delay } = result.value || result.reason;
-      if (result.status === "fulfilled") {
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
         Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-      } else {
+      })
+      .catch(({ position, delay }) => {
         Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-      }
-    });
-  });
+      });
+  }
 });
